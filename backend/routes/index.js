55a2a6b340971a3b1425117b561891537732b7cc
Akhil Nautiyal <akhilnautiyal5@gmail.com>
2024-09-22 10:02:06 +0530

    updated backend code

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var bcrypt = require("bcryptjs");
+var bcrypt = require("bcrypt");
 var userModel = require("../models/userModel");
 var projectModel = require("../models/projectModel");
 var jwt = require("jsonwebtoken");
